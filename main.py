bf00f3370a91f1348e114044d6e4a1666af521c5
Stefn93 <steffen.schneider02@gmail.com>
2024-03-15 10:43:06 +0100

    Programm überarbeitet

--- main.py
+++ main.py
@@ -1,18 +1,65 @@
-from tkinter import *
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import re
+import datetime
+import psycopg2
+import time
 
-temperaturenListe = [5, 19, 2, 5, 3, 8, 12, 3, 24, 5, 6, 5, 8, 4, 12, 13, 16, 9, 11, 10, 4, 7, 12, 0]
+#path = "c:/users/sschneider/test.txt"
+path = "/sys/bus/w1/devices/28-0000092b9435/w1_slave"
+temp = 0
+zeit = datetime.datetime.now()
+zeitformat = zeit.strftime("%H:%M:%S")
 
-fenster = Tk()
-fenster.title("Temperaturmesser")
-fenster.geometry("500x500")
+conn = psycopg2.connect(
+        dbname="tempsens",
+        user="postgres",
+        password="AdminAdmin",
+        host="127.0.0.1",
+        port="5432"
+    )
 
-uhrzeiten = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+def getTemperatureFromFile():
+    # Die aktuelle Zeit wo die Temperatur gemessen wurde
+    zeit = datetime.datetime.now()
+    zeitformat = zeit.strftime("%H:%M:%S")
+    with open(path, 'r') as file:
+        # Durch die Datei Zeile für Zeile iterieren
+        for line in file:
+            # Durchsuche die Zeile nach dem Muster 't=' gefolgt von Zahlen
+            match = re.search(r't=(\d+)', line)
+            # Falls ein Match gefunden wurde
+            if match:
+                # Extrahiere den gefundenen Wert und entferne das 't='
+                wert = match.group(1)
+                print("Gefundene Temperatur: ", wert)
+                temp = wert
+                # Hier kannst du weitere Verarbeitungsschritte durchführen, z.B. Wert in eine Variable speichern
 
-plt.plot(uhrzeiten, temperaturenListe)
-plt.title("Temperaturmesser")
-plt.scatter(uhrzeiten, temperaturenListe, color='red')
-plt.xlabel("Stunde")
-plt.ylabel("Temperaturen (°C)")
-plt.show()
+
+def writeToDatabase():
+    cur = conn.cursor()
+
+    # SQL-Befehl zum Einfügen eines Werts
+    sql = "INSERT INTO data (temperature, time) VALUES (%s)"
+
+    # Wert, den du einfügen möchtest
+    wert1 = temp
+    wert2 = zeitformat
+
+    # Wert in die Datenbank einfügen
+    cur.execute(sql, (wert1, wert2))
+
+    # Änderungen in der Datenbank bestätigen
+    conn.commit()
+
+    # Cursor schließen
+    cur.close()
+
+    # Verbindung zur Datenbank schließen
+    conn.close()
+
+def runProgram():
+    while True:
+        getTemperatureFromFile()
+        writeToDatabase()
+        time.sleep(60)
+
